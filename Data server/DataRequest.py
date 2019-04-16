3bd66eff37edf44418b09bee9cee754c720f3a23
Giacomo Carlotta <jack.carlotta2000@gmail.com>
2019-04-16 11:12:05 +0200

    DataRequest socket

--- Data server/DataRequest.py
+++ Data server/DataRequest.py
@@ -1,13 +1,14 @@
 #riceve n-sensore (SN), data type (DT), first e last time (FT,LT), si connette, crea un json e lo invia tramite socket al data server
 #2-websocket in ascolto riceve dati dal client web, e lo manda all'altra classe per poi inviarlo al data server
 import socket
+import json
 
 class DataRequest():
     def __init__(self,):
         with open(file = "./file/Request_settings.json", mode = 'r') as settingsFile:
             settings = json.load(fp = settingsFile)
         self.address = settings["address"]
-        self.port = settings["port"]
+        self.port = int(settings["port"])
 
 
 
@@ -15,12 +16,28 @@ class DataRequest():
         message = {"SN" : SN, "DT" : DT, "FT" : FT, "LT" : LT}
         jMessage = json.dumps(message)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((self.address, self.port))
+        s.connect((self.address, self.port))
         s.sendall(jMessage.encode())
         recieved = ''
-        while data.decode() != ''
-            data = s.recv(1024)
-            recieved += data.decode()
-        return recieved
+        try:
+            while True:
+                data = s.recv(1024)
+                if data.decode() == '200':
+                    break
+                recieved += data.decode()
+            s.close()
+            return recieved
+        except KeyboardInterrupt:
+            s.close()
+            return
 
-        
+
+def main():
+    req = DataRequest()
+    response = req.request(3,"temperatura",'2019-03-28 19:00:00','2019-03-28 22:00:00')
+    if response == '':
+        print('non cè micca niente')
+    print(response)
+
+if __name__ == '__main__':
+    main()
