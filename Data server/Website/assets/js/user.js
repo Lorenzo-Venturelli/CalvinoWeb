e269fd0b792be87e961417f79d4c9d2eddf8bbbf
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2019-05-13 09:23:14 +0200

    JS

--- Data server/Website/assets/js/user.js
+++ Data server/Website/assets/js/user.js
@@ -1,9 +1,25 @@
 var ws = WebSocket("wss://tggstudio.eu:8888/ws");
+var ctx = document.getElementById('graph').getContext('2d');
+var chart = new Chart(ctx, {
+    type: 'line',
+});
+
+function setDefault(){
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    document.getElementById("GFTD").defaultValue = yyyy + "-" + mm + "-" + dd;
+    document.getElementById("GLTD").defaultValue = yyyy + "-" + mm + "-" + dd;
+    document.getElementById("GFTT").defaultValue = "00:00";
+    document.getElementById("GLTT").defaultValue = "01:00";
+}
 
 function RTsetSensor(){
     var sensorNumber = document.getElementById("RTSN");
     var value = sensorNumber.options[sensorNumber.selectedIndex].text;
-
+    console.log(value)
     /* Interfaccia WebSocket richiesta */
 }
 
@@ -20,6 +36,24 @@ function RTupdate(temp, light, pressure, highness){
     return;
 }
 
+function GDrequest(){
+    var x = document.getElementById("GSN");
+    var sensorNumber = x.options[x.selectedIndex].text;
+    x = document.getElementById("GDT");
+    var dataType = x.options[x.selectedIndex].text;
+    var Fdata = document.getElementById("GFTD").value
+    var Ldata = document.getElementById("GLTD").value
+    var Ftime = document.getElementById("GFTT").value
+    var Ltime = document.getElementById("GLTT").value
+
+    /* Interfaccia WebSocket richiesta */
+}
+
 ws.addEventListener("message", function (message){
-    RTupdate(message["temp"], message["light"], message["pressure"], message["highness"]);
+    if (message["type"] == "RTD"){
+        RTupdate(message["temp"], message["light"], message["pressure"], message["highness"]);
+    }
+    else{
+        //
+    }
 });
