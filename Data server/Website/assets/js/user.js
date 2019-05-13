30656aabb85442ea0fbc2722568c0186c7e36d57
Lorenzo Venturelli <venturelli.lorenzo@outlook.it>
2019-05-13 18:21:11 +0200

    JS

--- Data server/Website/assets/js/user.js
+++ Data server/Website/assets/js/user.js
@@ -1,8 +1,5 @@
 var ws = WebSocket("wss://tggstudio.eu:8888/ws");
 var ctx = document.getElementById('graph').getContext('2d');
-var chart = new Chart(ctx, {
-    type: 'line',
-});
 
 function setDefault(){
     var today = new Date();
@@ -14,13 +11,15 @@ function setDefault(){
     document.getElementById("GLTD").defaultValue = yyyy + "-" + mm + "-" + dd;
     document.getElementById("GFTT").defaultValue = "00:00";
     document.getElementById("GLTT").defaultValue = "01:00";
+
+    buildGraph();
 }
 
 function RTsetSensor(){
     var sensorNumber = document.getElementById("RTSN");
     var value = sensorNumber.options[sensorNumber.selectedIndex].text;
-    console.log(value)
-    /* Interfaccia WebSocket richiesta */
+    sendRTupdate(value);
+    return
 }
 
 function RTupdate(temp, light, pressure, highness){
@@ -32,7 +31,6 @@ function RTupdate(temp, light, pressure, highness){
     element.nodeValue = pressure;
     element = document.getElementById("rtHighness");
     element.nodeValue = highness
-
     return;
 }
 
@@ -46,7 +44,53 @@ function GDrequest(){
     var Ftime = document.getElementById("GFTT").value
     var Ltime = document.getElementById("GLTT").value
 
-    /* Interfaccia WebSocket richiesta */
+    var firstTime = Fdata + " " + Ftime + ":00"
+    var lastTime = Ldata + " " + Ltime + ":00"
+
+    sendGDrequest(sensorNumber, dataType, firstTime, lastTime);
+    return;
+}
+
+function sendRTupdate(sensorNumber){
+    ws.send({"realTimeSN" : sensorNumber});
+    return;
+}
+
+function sendGDrequest(sensorNumber, dataType, firstTime, lastTime){
+    ws.send({"grapRequest" : [{"SN" : sensorNumber, "DT" : dataType, "FT" : firstTime, "LT" : lastTime}]});
+    return;
+}
+
+function buildGraph(){
+    var datas = [20.1, 22, 30, 20, 21, 20.1];
+    var dataType = "Temperatura in °C"
+    var minValue = Math.min(...datas) - 2;
+
+
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: dataType,
+            data: datas,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+            ],
+            borderWidth: 1
+        }]
+            },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false,
+                        min : minValue
+                    }
+                }]
+            }
+        }
+    });
 }
 
 ws.addEventListener("message", function (message){
